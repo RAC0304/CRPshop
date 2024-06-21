ad725cc017c988ecca9fe29af295b2b0371a9849
Rendy Ariesta Chandra <rendyariesta0@gmail.com>
2024-06-21 16:23:01 +0700

    ubah ke php

---
+++ checkout.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Checkout - ML</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat"
+      rel="stylesheet"
+    />
+    <link rel="icon" type="image/x-icon" href="./img/logo.png" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" href="belanja/checkout.css" />
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+          <a class="navbar-brand" href="index.html">
+            <img class="logo" src="img/logoputih.png" alt="Logo" />
+          </a>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">
+                Home
+                <i class="fa fa-home"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="belanja/mobilelegends.html">
+                Daftar Harga
+                <i class="fa fa-dollar"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <main class="container">
+      <h1 class="title__list">
+        CHECKOUT <span class="th">TOPUP GAME ONLINE CRPshop</span>
+      </h1>
+      <div class="table-container">
+        <table class="table">
+          <tr>
+            <th>Id Game:</th>
+            <td>123131</td>
+          </tr>
+          <tr>
+            <th>Nominal:</th>
+            <td>1425 DIAMOND</td>
+          </tr>
+          <tr>
+            <th>Pembayaran:</th>
+            <td>OVO</td>
+          </tr>
+          <tr>
+            <th>Total Harga:</th>
+            <td>Rp 148,500</td>
+          </tr>
+        </table>
+      </div>
+      <div class="order-button">
+        <button type="button" class="btn">KLIK UNTUK MEMESAN</button>
+      </div>
+    </main>
+
+    <footer>
+      <p>&copy; 2024 CRPshop. All rights reserved.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
